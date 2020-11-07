e8869aa9334f7e6ea09fbb32983be5b069c3c0df
John Emmanuel Bacalla <johnemmanuel.bacalla@gmail.com>
2020-11-07 20:13:21 +0800

    Display current user's domain name in navbar account and add an icon

--- src/components/ORHeaderAccount.js
+++ src/components/ORHeaderAccount.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { Menu } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
 
-export default function ORHeaderAccount() {
+const ORHeaderAccount = (props) => {
     return (
         <Menu theme="light" mode="horizontal" style={{ float: "right" }}>
-            <SubMenu title="Account">
+            <SubMenu icon={<UserOutlined />} title={props.account.domainName}>
                 <Menu.Item>Logout</Menu.Item>
             </SubMenu>
         </Menu>
     );
-}
+};
+
+const mapStateToProps = (state) => ({ account: state.auth.account });
+
+const mapDispatchToProps = () => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ORHeaderAccount);
