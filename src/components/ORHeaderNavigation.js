4a814c2fab1419643a89d39324dbf229d4dd58a5
John Emmanuel Bacalla <johnemmanuel.bacalla@gmail.com>
2020-11-07 20:21:38 +0800

    Add icons to header links

--- src/components/ORHeaderNavigation.js
+++ src/components/ORHeaderNavigation.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 import { Menu } from "antd";
+import {
+    AppstoreOutlined,
+    BlockOutlined,
+    DashboardOutlined,
+} from "@ant-design/icons";
 
 export default function ORHeaderNavigation() {
     return (
@@ -10,9 +15,15 @@ export default function ORHeaderNavigation() {
             defaultSelectedKeys={["1"]}
             style={{ float: "left" }}
         >
-            <Menu.Item key="1">Dashboard</Menu.Item>
-            <Menu.Item key="2">Releases</Menu.Item>
-            <Menu.Item key="3">Stories</Menu.Item>
+            <Menu.Item icon={<DashboardOutlined />} key="1">
+                Dashboard
+            </Menu.Item>
+            <Menu.Item icon={<AppstoreOutlined />} key="2">
+                Releases
+            </Menu.Item>
+            <Menu.Item icon={<BlockOutlined />} key="3">
+                Stories
+            </Menu.Item>
         </Menu>
     );
 }
