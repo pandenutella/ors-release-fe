df3b839ad7b71c1665be70bee60112e2abb92dbf
John Emmanuel Bacalla <johnemmanuel.bacalla@gmail.com>
2020-11-07 18:54:44 +0800

    Create empty home page and setup routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
 
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
 
 function App() {
-    return <LoginPage />;
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <HomePage />
+                </Route>
+                <Route path="/login">
+                    <LoginPage />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
