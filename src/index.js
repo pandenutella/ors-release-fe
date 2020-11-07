3d6106202d3c3ea13abc7fc7bcfa725bed0281dd
John Emmanuel Bacalla <johnemmanuel.bacalla@gmail.com>
2020-11-07 15:42:05 +0800

    Setup redux for login

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "antd/dist/antd.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import store from "./store";
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/reducers/accounts.js
@@ -0,0 +1,8 @@
+const initialAccounts = [{ username: "bacaljo", password: "123" }];
+
+export default function accounts(state = initialAccounts, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
